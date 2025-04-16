37ed9917ad135323784a05f1d7e223f45e45d17f
ATRedshaw <redshawalex02@outlook.com>
2025-04-16 10:50:24 +0100

    Updated lat and long to use api; initialised holidays and preprocess files

--- models/crowd-level/utils/geo.py
+++ models/crowd-level/utils/geo.py
@@ -5,22 +5,41 @@ from meteostat import Point, Daily
 import pandas as pd
 import requests
 
-def get_lat_long(park_id):
+def get_lat_long(park_id, api_url='https://queue-times.com/parks.json'):
     """Use the park_id to get the longitude and latitude of the park from the park_locations.jsomn file.
     
     Args:
-        park_id (str): The park_id of the park.
+        park_id (int): The park_id of the park.
         
     Returns:
         tuple: A tuple containing the longitude and latitude of the park.
     """
-    park_locations = json.load(open('models/park_locations.json'))
-    if park_id in park_locations:
-        longitude = park_locations[park_id]["longitude"]
-        latitude = park_locations[park_id]["latitude"]
-        return (latitude, longitude)
-    else:
-        raise ValueError("Invalid park_id provided.")
+
+    try:
+        park_id = int(park_id)
+    except:
+        pass
+
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        parks_data = response.json()
+
+        for company in parks_data:
+            for park in company.get("parks", []):
+                if park["id"] == park_id:
+                    latitude = float(park["latitude"])
+                    longitude = float(park["longitude"])
+                    return longitude, latitude
+
+        print(f"Error: Park ID {park_id} not found.")
+        return ()
+    except requests.RequestException as e:
+        print(f"Error fetching park data: {e}")
+        return ()
+    except (ValueError, KeyError) as e:
+        print(f"Error processing park data: {e}")
+        return ()
 
 def get_weather_data(start_date, end_date, latitude, longitude, is_model_training=True):
     """
@@ -168,3 +187,7 @@ def get_weather_data(start_date, end_date, latitude, longitude, is_model_trainin
         return model_training(start_date, end_date, longitude, latitude)
     else:
         return model_inference(start_date, end_date, longitude, latitude)
+
+if __name__ == "__main__":
+    park_id = "2"
+    print(get_lat_long(park_id))
